c49615e0843cd168b32aba34755961cd2eb712fa
AImm <neilguo@aimatchmaker.com>
2020-05-15 14:51:31 +0800

    first commit

---
+++ lib/screens/meal_detail_screen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import '../dummy_data.dart';
+
+class MealDetailScreen extends StatelessWidget {
+  static const routeName = '/meals-detail';
+
+  Widget _buildSectionTitle(BuildContext context, String text) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        text,
+        style: Theme.of(context).textTheme.title,
+      ),
+    );
+  }
+
+  Widget _buildContainer(Widget child) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      height: 100,
+      width: 300,
+      child: child,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final id = ModalRoute.of(context).settings.arguments as String;
+    final selectedMeal = DUMMY_MEALS.firstWhere((meal) {
+      return meal.id == id;
+    });
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('${selectedMeal.title}'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                selectedMeal.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            _buildSectionTitle(context, 'Ingredients'),
+            _buildContainer(
+              ListView.builder(
+                  itemBuilder: (ctx, index) => Card(
+                        color: Theme.of(context).accentColor,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 5,
+                            horizontal: 10,
+                          ),
+                          child: Text(selectedMeal.ingredients[index]),
+                        ),
+                      ),
+                  itemCount: selectedMeal.ingredients.length),
+            ),
+            _buildSectionTitle(context, 'Step'),
+            _buildContainer(
+              ListView.builder(
+                  itemBuilder: (ctx, index) => Column(
+                        children: <Widget>[
+                          ListTile(
+                            leading: CircleAvatar(
+                              child: Text('# ${index + 1}'),
+                            ),
+                            title: Text(selectedMeal.steps[index]),
+                          ),
+                          Divider(),
+                        ],
+                      ),
+                  itemCount: selectedMeal.steps.length),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import './categories_screen.dart';
+import './favorites_screen.dart';
+import '../widgets/main_drawer.dart';
+class TabsScreen extends StatefulWidget {
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = [
+    {
+      'page': CategoriesScreen(),
+      'title': 'Categories',
+    },
+    {
+      'page': FavoritesScreen(),
+      'title': 'Your Favorite',
+    },
+  ];
+  int _selectedIndex = 0;
+  void _selectedPage(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedIndex]['title']),
+      ),
+      drawer: MainDrawer(),
+      body: _pages[_selectedIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectedPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedIndex,
+        selectedFontSize: 13,
+        unselectedFontSize: 13,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            title: Text('Categories'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            title: Text('Favorites'),
+          )
+        ],
+      ),
+    );
+  }
+}
